32bb61a11371214ba530c1849c3b51cee2b7d3f4
SeungEun Baek <sebaek@gmail.com>
2023-02-16 14:13:42 +0900

    1008

---
+++ src/ch03/lecture/p01arithmetic/C03Arithmetic.java
@@ -0,0 +1,5 @@
+package ch03.lecture.p01arithmetic;
+
+public class C03Arithmetic {
+
+}
--- src/ch03/sec02/ArithmeticOperatorExample.java
+++ src/ch03/sec02/ArithmeticOperatorExample.java
@@ -13,7 +13,7 @@ public class ArithmeticOperatorExample {
 		long result2 = v1 + v2 - v4;
 		System.out.println("result2: " + result2);
 		
-		double result3 = (double) v1 / v2;
+		double result3 = ((double) v1) / v2;
 		System.out.println("result3: " + result3);
 		
 		int result4 = v1 % v2;
