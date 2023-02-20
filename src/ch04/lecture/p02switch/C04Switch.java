bd6d866d91696d328100c251cc6809c76616b3b2
SeungEun Baek <sebaek@gmail.com>
2023-02-20 12:38:18 +0900

    switch

--- src/ch04/lecture/p02switch/C04Switch.java
+++ src/ch04/lecture/p02switch/C04Switch.java
@@ -2,8 +2,6 @@ package ch04.lecture.p02switch;
 
 public class C04Switch {
 	public static void main(String[] args) {
-		// switch labeled statement
-		// break 없이 코드 블럭 작성 가능
 
 		int val = 4;
 
@@ -23,6 +21,8 @@ public class C04Switch {
 			break;
 		}
 
+		// switch labeled statement
+		// break 없이 코드 블럭 작성 가능
 		switch (val) {
 		case 1 -> System.out.println("code 1"); // 명령문 하나일 때 중괄호 생략 가능
 
