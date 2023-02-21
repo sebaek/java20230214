eeab30559316d4c969d1135b71276143e780a4a0
SeungEun Baek <sebaek@gmail.com>
2023-02-21 12:28:53 +0900

    Update Example04.java

--- src/ch04/exercise/Example04.java
+++ src/ch04/exercise/Example04.java
@@ -6,13 +6,16 @@ public class Example04 {
 		int num1;
 		int num2;
 
-		do {
-			num1 = ((int) (Math.random() * 6)) + 1;
-			num2 = ((int) (Math.random() * 6)) + 1;
-
-			System.out.printf("(%d, %d)%n", num1, num2);
-
-		} while ((num1 + num2) != 5);
+		while (true) {
+			num1 = (int) (Math.random() * 6) + 1;
+			num2 = (int) (Math.random() * 6) + 1;
+			
+			System.out.printf("(%d, %d) %n", num1, num2);
+			
+			if ((num1 + num2) == 5) {
+				break;
+			}
+		}
 
 	}
 }
