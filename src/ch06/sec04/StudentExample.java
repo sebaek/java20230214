f5d2da78b339ec2988f93a70b325922a31c05331
SeungEun Baek <sebaek@gmail.com>
2023-02-27 12:18:44 +0900

    student

--- src/ch06/sec04/StudentExample.java
+++ src/ch06/sec04/StudentExample.java
@@ -7,5 +7,18 @@ public class StudentExample {
 		
 		Student s2 = new Student();
 		System.out.println("s2 변수가 또 다른 Student 객체를 참조합니다.");
+		
+		System.out.println(s1 == s2); // false
+		
+		// 참조값얻기 : System.identityHashCode
+		System.out.println(System.identityHashCode(s1));
+		System.out.println(System.identityHashCode(s2));
 	}
 }
+
+
+
+
+
+
+
