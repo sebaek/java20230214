107950e924beef6d233be53831d04af07e2a9b02
SeungEun Baek <sebaek@gmail.com>
2023-02-27 11:46:41 +0900

    class

---
+++ src/ch06/lecture/p01class/C01Class.java
@@ -0,0 +1,24 @@
+package ch06.lecture.p01class;
+
+public class C01Class {
+	public static void main(String[] args) {
+		MyClass01 var1 = new MyClass01();
+		MyClass01 var2 = new MyClass01();
+		
+		
+		
+	}
+}
+
+// 클래스 작성
+class MyClass01 {
+	// 어떤 값을 가져야 하는지? (필드)
+	
+	// 어떤 기능이 있어야 하는지? (메소드)
+	
+	
+}
+
+
+
+
