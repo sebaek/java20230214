4c062f202a61b372d5c51d8c4564779f6b537748
SeungEun Baek <sebaek@gmail.com>
2023-02-27 15:21:56 +0900

    파라미터

---
+++ src/ch06/lecture/p03method/C04Parameter.java
@@ -0,0 +1,32 @@
+package ch06.lecture.p03method;
+
+public class C04Parameter {
+	public static void main(String[] args) {
+		MyClass04 o1 = new MyClass04();
+		o1.method1();
+		
+		// 메소드실행시 정의된 파라미터 타입과 순서, 개수에 맞춰서
+		// 아규먼트 입력(전달)
+//		o1.method1(3); // 안됨
+		
+		o1.method2(5);
+//		o1.method2(); // 안됨
+		
+		o1.method3("hello");
+		
+		String s1 = "java";
+		
+		o1.method3(s1);
+		
+		o1.method3(null);
+		
+		String s2 = null;
+		
+		o1.method3(s2);
+	}
+}
+
+
+
+
+
