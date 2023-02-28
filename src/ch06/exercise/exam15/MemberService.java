417404e643abdfcacb7413137fd68539ba25ffa3
SeungEun Baek <sebaek@gmail.com>
2023-02-28 11:17:45 +0900

    확인문제 15

---
+++ src/ch06/exercise/exam15/MemberService.java
@@ -0,0 +1,16 @@
+package ch06.exercise.exam15;
+
+public class MemberService {
+
+	public boolean login(String id, String password) {
+		return id.equals("hong") && password.equals("12345");
+	}
+
+	public void logout(String id) {
+
+		System.out.println(id + "님이 로그아웃 되었습니다.");
+		
+	}
+
+
+}
