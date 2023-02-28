ccb7bdbb8a880fdcf4f51bfc30ada53da9f31bd3
SeungEun Baek <sebaek@gmail.com>
2023-02-28 12:37:27 +0900

    Update Solution120843.java

--- src/codetest/programmers/Solution120843.java
+++ src/codetest/programmers/Solution120843.java
@@ -2,7 +2,6 @@ package codetest.programmers;
 
 public class Solution120843 {
     public int solution(int[] numbers, int k) {
-        int answer = 0;
-        return answer;
+        return numbers[(k - 1) * 2 % numbers.length];
     }
 }
