f7c411fe01738854a5b963dcbe1b5ce6809c58e4
SeungEun Baek <sebaek@gmail.com>
2023-03-06 15:14:49 +0900

    확인문제 12

---
+++ src/ch07/exercise/exam12/Example.java
@@ -0,0 +1,21 @@
+package ch07.exercise.exam12;
+
+public class Example {
+	public static void main(String[] args) {
+		action(new A());
+		action(new B());
+		action(new C());
+	}
+	
+	public static void action(A a) {
+		a.method1();
+		
+		if (a instanceof C c) {
+//			C c = (C) a;
+			c.method2();
+		}
+	}
+}
+
+
+
