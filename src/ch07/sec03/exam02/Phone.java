320e57f4ed4b78c1bb49d644dd2a39acc9a2e0f9
SeungEun Baek <sebaek@gmail.com>
2023-03-07 10:56:05 +0900

    상위 클래스 생성자 호출

---
+++ src/ch07/sec03/exam02/Phone.java
@@ -0,0 +1,14 @@
+package ch07.sec03.exam02;
+
+public class Phone {
+
+	public String model;
+	public String color;
+	
+	public Phone(String model, String color) {
+		this.model = model;
+		this.color = color;
+		
+		System.out.println("Phone(String, String) 생성자 호출");
+	}
+}
