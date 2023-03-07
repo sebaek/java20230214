8e178c3ec937984f14b4dfb3f3fa8fb8709bafba
SeungEun Baek <sebaek@gmail.com>
2023-03-07 11:31:34 +0900

    상위클래스 멤버 접근

---
+++ src/ch07/lecture/p08super/C04Super.java
@@ -0,0 +1,33 @@
+package ch07.lecture.p08super;
+
+public class C04Super {
+
+}
+
+class Super04 {
+	public void method1() {
+		
+	}
+	public void method3() {
+		
+	}
+}
+
+class Sub04 extends Super04 {
+	@Override
+	public void method3() {
+		super.method1();
+		method3();// 자기 자신
+		super.method3(); // 부모클래스의 method3
+	}
+	public void method2() {
+		super.method1();
+	}
+}
+
+
+
+
+
+
+
