e74887752d86d8b125f903f20f8750b6f573533a
SeungEun Baek <sebaek@gmail.com>
2023-03-07 12:28:41 +0900

    Interface

---
+++ src/ch08/lecture/p01interface/MyClass01.java
@@ -0,0 +1,9 @@
+package ch08.lecture.p01interface;
+
+
+// 인터페이스 구현 클래스
+// 클래스명 implements 인터페이스명...
+// 인터페이스를 여러개 구현 할 수 있다.
+public class MyClass01 implements MyInterface01, MyInterface02 {
+
+}
