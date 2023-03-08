b5b1b5c0e044cee7ea5d780f197e628648eff437
SeungEun Baek <sebaek@gmail.com>
2023-03-08 15:41:46 +0900

    확인문제6

---
+++ src/ch12/exercise/exam06/Member.java
@@ -0,0 +1,17 @@
+package ch12.exercise.exam06;
+
+public class Member {
+	private String id;
+	private String name;
+
+	public Member(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return id + ":" + name;
+	}
+}
