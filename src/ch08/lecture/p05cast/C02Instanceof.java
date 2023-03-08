0c9df14f4b2155a7e6fe37d7d55745ba8981b6ca
SeungEun Baek <sebaek@gmail.com>
2023-03-08 11:38:57 +0900

    instanceof

--- src/ch08/lecture/p05cast/C02Instanceof.java
+++ src/ch08/lecture/p05cast/C02Instanceof.java
@@ -5,10 +5,17 @@ public class C02Instanceof {
 		MyInterface02 o1 = new MyClass021();
 		MyInterface02 o2 = new MyClass022();
 		
-		MyClass021 o3 = (MyClass021) o1; // ok
-		MyClass022 o4 = (MyClass022) o2; // ok
+		if (o1 instanceof MyClass021) {
+			MyClass021 o3 = (MyClass021) o1; // ok
+		}
 		
-		MyClass022 o5 = (MyClass022) o1; // x
+		if (o2 instanceof MyClass022) {
+			MyClass022 o4 = (MyClass022) o2; // ok
+		}
+		
+		if (o1 instanceof MyClass022) {
+			MyClass022 o5 = (MyClass022) o1; // x
+		}
 		
 		System.out.println("실행 흐름 이어감");
 		
