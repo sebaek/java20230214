1be3f0cd0ef4917cad6c72770566ed91a5551e09
SeungEun Baek <sebaek@gmail.com>
2023-03-08 09:23:52 +0900

    default method

---
+++ src/ch08/lecture/p04default_method/MyClass012.java
@@ -0,0 +1,8 @@
+package ch08.lecture.p04default_method;
+
+public class MyClass012 implements MyInterface01 {
+	@Override
+	public void method1() {
+		System.out.println("class012 override");
+	}
+}
