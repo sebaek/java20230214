274b0f2ddd032f06771db5e98487543e800fa361
SeungEun Baek <sebaek@gmail.com>
2023-03-08 09:46:11 +0900

    디폴트 메소드 재정의

--- src/ch08/lecture/p04default_method/MyClass012.java
+++ src/ch08/lecture/p04default_method/MyClass012.java
@@ -5,4 +5,10 @@ public class MyClass012 implements MyInterface01 {
 	public void method1() {
 		System.out.println("class012 override");
 	}
+	
+	// default 메소드도 재정의 가능
+	@Override
+	public void method2() {
+		System.out.println("class012에서 default 메소드 method2 재정의");
+	}
 }
