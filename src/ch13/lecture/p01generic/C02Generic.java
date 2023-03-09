73d89c877fff3b7058e1d041873e4ec59ed760b5
SeungEun Baek <sebaek@gmail.com>
2023-03-09 15:15:57 +0900

    generic type

--- src/ch13/lecture/p01generic/C02Generic.java
+++ src/ch13/lecture/p01generic/C02Generic.java
@@ -1,11 +1,24 @@
 package ch13.lecture.p01generic;
 
+import java.util.*;
+
 public class C02Generic {
 	public static void main(String[] args) {
 		MyClass02<String> o1 = new MyClass02<String>();
 		o1.setItem("java");
 		String s1 = o1.getItem();
 		System.out.println(s1.length());
+		
+		MyClass02<Scanner> o2 = new MyClass02<Scanner>();
+		o2.setItem(new Scanner(""));
+		Scanner sc = o2.getItem();
+		System.out.println(sc.hasNext());
+		
+		// 기본타입은 타입아규먼트로 사용불가하므로 wrapper type 사용
+		MyClass02<Integer> o3 = new MyClass02<Integer>();
+		o3.setItem(3); // autoboxing
+		int i = o3.getItem(); // unboxing
+		System.out.println(i);
 	}
 }
 
