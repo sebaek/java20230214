946b35e1c42d6e953aac285582fe6f8c7c908c28
SeungEun Baek <sebaek@gmail.com>
2023-03-09 12:37:50 +0900

    Math

---
+++ src/ch05/exercise/Exercise07Max.java
@@ -0,0 +1,15 @@
+package ch05.exercise;
+
+public class Exercise07Max {
+	public static void main(String[] args) {
+		int[] array = {1, 5, 3, 8, 2};
+		
+		int max = Integer.MIN_VALUE;
+		
+		for (int n : array) {
+			max = Math.max(max, n);
+		}
+		
+		System.out.println(max);
+	}
+}
