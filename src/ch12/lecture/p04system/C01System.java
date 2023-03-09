dfd21c37b989af9f7ea9ae9b5d65598d4aa95ce9
SeungEun Baek <sebaek@gmail.com>
2023-03-09 14:37:32 +0900

    system

---
+++ src/ch12/lecture/p04system/C01System.java
@@ -0,0 +1,12 @@
+package ch12.lecture.p04system;
+
+public class C01System {
+	public static void main(String[] args) {
+		// System
+		// currentTimeMillis
+		// 현재시간 (1970년 1월 1일 0시부터 현재까지의 milli second 경과)
+		
+		long current = System.currentTimeMillis();
+		System.out.println(current);
+	}
+}
