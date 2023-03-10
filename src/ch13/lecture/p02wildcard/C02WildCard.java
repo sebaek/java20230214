d3495ad96db5c31e55715ed8fc06c02701c5b143
SeungEun Baek <sebaek@gmail.com>
2023-03-10 11:26:48 +0900

    Update C02WildCard.java

--- src/ch13/lecture/p02wildcard/C02WildCard.java
+++ src/ch13/lecture/p02wildcard/C02WildCard.java
@@ -5,8 +5,7 @@ public class C02WildCard {
 		// 값이 나갈 때(out) super
 		MyClass02<? super String> o1 = new MyClass02<String>();
 		o1.item = new String(); // String 또는 그 하위타입을 대입 가능
-		
-//		String s1 = o1.item; // String에 할당 불가
+		String s1 = o1.item; // String에 할당 불가
 		
 		// 값이 들어올 때(in) extends
 		MyClass02<? extends String> o2 = new MyClass02<String>();
