fd95f8731e1cdd584aafc91368518dcfbb2c9b57
SeungEun Baek <sebaek@gmail.com>
2023-03-10 15:17:00 +0900

    local class

---
+++ src/ch09/exercise/exam04/CarExample.java
@@ -0,0 +1,11 @@
+package ch09.exercise.exam04;
+
+public class CarExample {
+	public static void main(String[] args) {
+		Car.Engine engine = new Car.Engine();
+		
+		Car car = new Car();
+		
+		Car.Tire tire = car.new Tire();
+	}
+}
---
+++ src/ch09/lecture/p01nested/C04LocalClass.java
@@ -0,0 +1,23 @@
+package ch09.lecture.p01nested;
+
+public class C04LocalClass {
+
+	void method1() {
+		// 로컬 클래스 (local class)
+		// 선언된 메소드 내에서만 사용 가능
+		class LocalClass1 {
+			
+		}
+		
+		LocalClass1 o1 = new LocalClass1();
+		
+		
+	}
+	
+	void method2() {
+		LocalClass1 o1 = new LocalClass1(); // x
+	}
+}
+
+
+
