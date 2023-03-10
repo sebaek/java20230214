5891f572cb1503db3315b8a587e42bd942ac9c0f
SeungEun Baek <sebaek@gmail.com>
2023-03-10 11:34:24 +0900

    wildcard

---
+++ src/ch13/lecture/p02wildcard/C03WildCard.java
@@ -0,0 +1,22 @@
+package ch13.lecture.p02wildcard;
+
+public class C03WildCard {
+	public static void main(String[] args) {
+		MyClass03<? super Number> o1 = new MyClass03<Number>();
+		MyClass03<? super Number> o2 = new MyClass03<Object>();
+//		o1.item = new Number(); //됨 (추상 클래스여서 인스턴스화못함)
+		o1.item = new Integer(0); // ok
+		
+		MyClass03<? extends Number> o3 = new MyClass03<Number>();
+		MyClass03<? extends Number> o4 = new MyClass03<Integer>();
+		MyClass03<? extends Number> o6 = new MyClass03<Double>();
+		Number n1 = o4.item; // ok
+		Object o5 = o4.item; // ok
+		Integer i1 = o4.item; // xx
+		
+	}
+}
+
+class MyClass03<T> {
+	public T item;
+}
