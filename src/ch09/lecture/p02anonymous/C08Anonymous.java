4049fa99389309ba34b821beb5f514eaed4c418f
SeungEun Baek <sebaek@gmail.com>
2023-03-13 09:18:22 +0900

    interface

---
+++ src/ch09/lecture/p02anonymous/C08Anonymous.java
@@ -0,0 +1,18 @@
+package ch09.lecture.p02anonymous;
+
+public class C08Anonymous {
+	public static void main(String[] args) {
+		MyInterface08 o1 = new MyInterface08() {
+			@Override
+			public void method1() {
+				System.out.println("메소드 재정의!!!!");
+			}
+		};
+		
+		o1.method1();
+	}
+}
+
+interface MyInterface08 {
+	void method1();
+}
