e9a3f8515e3650a8db6fb6cb9d1a1f8172f55ffc
SeungEun Baek <sebaek@gmail.com>
2023-03-13 11:17:01 +0900

    lambda

---
+++ src/ch16/lecture/p01anonymous/C02Lambda.java
@@ -0,0 +1,13 @@
+package ch16.lecture.p01anonymous;
+
+public class C02Lambda {
+	public static void main(String[] args) {
+		MyInterface02 o1 = () -> System.out.println("재정의한 메소드");
+		
+		
+	}
+}
+
+interface MyInterface02 {
+	void method1();
+}
