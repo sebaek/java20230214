c51eb5753eb0bd91cc7e7d80122b4ff229ee9baf
SeungEun Baek <sebaek@gmail.com>
2023-03-14 09:51:17 +0900

    확인문제9

--- src/ch16/exercise/exam08/Example.java
+++ src/ch16/exercise/exam08/Example.java
@@ -17,10 +17,15 @@ public class Example {
 	}
 	
 	public static void main(String[] args) {
-		double englishAvg = avg(s -> s.getEnglishScore());
+//		double englishAvg = avg(s -> s.getEnglishScore());
+		double englishAvg = avg(Student::getEnglishScore);
 		System.out.println("영어 평균 점수: " + englishAvg);
 		
-		double mathAvg = avg(s -> s.getMathScore());
+//		double mathAvg = avg(s -> s.getMathScore());
+		double mathAvg = avg(Student::getMathScore);
 		System.out.println("수학 평균 점수: " + mathAvg);
 	}
 }
+
+
+
