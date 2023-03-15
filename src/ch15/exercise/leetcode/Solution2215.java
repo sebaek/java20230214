16c3c76b05f19c7543e90b61dee98ec73717ab45
SeungEun Baek <sebaek@gmail.com>
2023-03-15 14:23:09 +0900

    리트코드 2215

---
+++ src/ch15/exercise/leetcode/Solution2215.java
@@ -0,0 +1,11 @@
+package ch15.exercise.leetcode;
+
+import java.util.*;
+
+public class Solution2215 {
+	public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+		//코드 작성
+		
+		return null;
+	}
+}
