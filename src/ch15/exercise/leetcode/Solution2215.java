fa279030183ef46181740ed76c88a850c47ffa56
SeungEun Baek <sebaek@gmail.com>
2023-03-15 15:17:23 +0900

    Update Solution2215.java

--- src/ch15/exercise/leetcode/Solution2215.java
+++ src/ch15/exercise/leetcode/Solution2215.java
@@ -5,7 +5,31 @@ import java.util.*;
 public class Solution2215 {
 	public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
 		//코드 작성
+		Set<Integer> set1 = new HashSet<>();
+		Set<Integer> set2 = new HashSet<>();
 		
-		return null;
+		for (int n : nums1) {
+			set1.add(n);
+		}
+		
+		for (int n : nums2) {
+			set2.add(n);
+		}
+		
+		List<Integer> list1 = new ArrayList<>();
+		List<Integer> list2 = new ArrayList<>();
+		
+		// code...
+		list1.addAll(set1);
+		list2.addAll(set2);
+		
+		list1.removeIf(set2::contains);
+		list2.removeIf(set1::contains);
+		
+		return List.of(list1, list2);
 	}
 }
+
+
+
+
