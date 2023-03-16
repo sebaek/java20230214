13134a8e640c258c1f01f2778cea866540dbdd65
SeungEun Baek <sebaek@gmail.com>
2023-03-16 14:53:19 +0900

    comparator

---
+++ src/ch15/lecture/p07treeset/C03Comparator.java
@@ -0,0 +1,58 @@
+package ch15.lecture.p07treeset;
+
+import java.util.*;
+
+public class C03Comparator {
+	public static void main(String[] args) {
+		TreeSet<Book> set = new TreeSet<>((b1, b2) -> b1.getTitle().compareTo(b2.getTitle()));
+		set.add(new Book("slamdunk"));
+		set.add(new Book("avatar"));
+		set.add(new Book("glory"));
+		
+		System.out.println(set);
+	}
+}
+
+// getter/setter, constructor, toString, hashcode/equals
+class Book {
+	private String title;
+
+	public Book(String title) {
+		this.title = title;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(title);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(title, other.title);
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Book [title=" + title + "]";
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+}
