2aef7c5224e77a83a5559fc22d0b958936801f82
SeungEun Baek <sebaek@gmail.com>
2023-03-20 14:42:50 +0900

    flatmap

---
+++ src/ch17/sec06/exam03/FlatMappingExample.java
@@ -0,0 +1,46 @@
+package ch17.sec06.exam03;
+
+import java.util.*;
+
+public class FlatMappingExample {
+	public static void main(String[] args) {
+		List<String> list1 = List.of(
+				"this is java",
+				"i am a best developer");
+
+		list1.stream()
+				.flatMap(data -> Arrays.stream(data.split(" ")))
+				.forEach(System.out::println);
+
+		System.out.println();
+
+		List<String> list2 = List.of("10, 20, 30", "40, 50");
+		list2.stream()
+				.flatMapToInt(data -> {
+					String[] strArr = data.split(",");
+					int[] intArr = new int[strArr.length];
+					for (int i = 0; i < strArr.length; i++) {
+						intArr[i] = Integer.parseInt(strArr[i].trim());
+					}
+					return Arrays.stream(intArr);
+				})
+				.forEach(System.out::println);
+		
+		System.out.println();
+		list2.stream()
+			.flatMap(s -> Arrays.stream(s.split(",")))
+			.map(s -> s.trim())
+			.mapToInt(Integer::parseInt)
+			.forEach(System.out::println);
+
+		System.out.println();
+		list2.stream()
+			.flatMap(s -> Arrays.stream(s.split("\\s*,\\s*")))
+			.mapToInt(Integer::parseInt)
+			.forEach(System.out::println);
+	}
+}
+
+
+
+
