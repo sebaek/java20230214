101eeda94cb04a29f7f466ffd994375b1dd67819
SeungEun Baek <sebaek@gmail.com>
2023-03-20 15:49:26 +0900

    sorted

---
+++ src/ch17/lecture/p03intermediate/C19Sorted.java
@@ -0,0 +1,16 @@
+package ch17.lecture.p03intermediate;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class C19Sorted {
+	public static void main(String[] args) {
+
+		IntStream.range(1, 6)
+
+				/* 중간연산들 작성 */
+				.boxed()
+				.sorted(Comparator.reverseOrder())
+				.forEach(System.out::println);
+	}
+}
