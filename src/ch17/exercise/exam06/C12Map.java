177c2b58735d6f5f9bbb7637f2d2d24eff419bce
SeungEun Baek <sebaek@gmail.com>
2023-03-20 12:40:38 +0900

    boxed

---
+++ src/ch17/exercise/exam06/C12Map.java
@@ -0,0 +1,23 @@
+package ch17.exercise.exam06;
+
+import java.util.stream.*;
+
+public class C12Map {
+	public static void main(String[] args) {
+		// 기본타입 int가 원소 스트림
+		IntStream stream1 = IntStream.range(11, 20);
+		
+		// 참조타입 Integer가 원소인 스트림으로 변환
+		Stream<Integer> stream2 = stream1.mapToObj(Integer::valueOf);
+		
+		long count = stream2.count();
+		
+		System.out.println(count);
+		
+		
+		
+	}
+}
+
+
+
