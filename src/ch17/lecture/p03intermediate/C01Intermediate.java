6c69f3e0b141b440b145fbf994523554bd56a565
SeungEun Baek <sebaek@gmail.com>
2023-03-20 09:40:58 +0900

    distinct, limit

---
+++ src/ch17/lecture/p03intermediate/C01Intermediate.java
@@ -0,0 +1,27 @@
+package ch17.lecture.p03intermediate;
+
+public class C01Intermediate {
+	public static void main(String[] args) {
+		// 스트림 API
+		// 1. 최종 연산 
+		// 리턴타입 : Stream아님
+		
+		// 2. 중간 연산
+		// 리턴타입 : Stream
+		// distinct : 중복 제거
+		// limit : 크기 제한
+		// sorted : 정렬
+		// filter : 필터링(솎아내기)
+		// map : 매핑 (다른 원소로 매핑)
+		// flatmap : 매핑 (다른 원소들로 매핑)
+		// boxed : 기본타입스트림 -> 참조타입스트림 변환
+		// peek : 중간 처리 연산 (foreach 와 비교됨)
+		
+		
+	}
+}
+
+
+
+
+
