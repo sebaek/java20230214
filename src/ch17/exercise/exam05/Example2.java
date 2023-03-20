814148c831860dad35501dc9440e232fed28f1ff
SeungEun Baek <sebaek@gmail.com>
2023-03-20 10:44:00 +0900

    확인문제 5

--- src/ch17/exercise/exam05/Example2.java
+++ src/ch17/exercise/exam05/Example2.java
@@ -2,21 +2,19 @@ package ch17.exercise.exam05;
 
 import java.util.*;
 
-public class Example1 {
+public class Example2 {
 	public static void main(String[] args) {
 		List<String> list = Arrays.asList(
 				"This is a java book",
 				"Lambda Expressions",
 				"Java8 supports lambda expressions");
 
-		for (String e : list) {
-			if (e.toLowerCase().contains("java")) {
-				System.out.println(e);
-			}
-		}
+		list.stream()
+				.filter(Example2::havingJava)
+				.forEach(System.out::println);
+	}
+
+	private static boolean havingJava(String s) {
+		return s.toLowerCase().contains("java");
 	}
 }
-
-
-
-
