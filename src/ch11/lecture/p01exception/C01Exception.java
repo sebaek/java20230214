20ee69a1d8f0b75b36aaced521124de731ae1e54
SeungEun Baek <sebaek@gmail.com>
2023-03-21 09:41:26 +0900

    checked exception, unchecked exception

---
+++ src/ch11/lecture/p01exception/C01Exception.java
@@ -0,0 +1,15 @@
+package ch11.lecture.p01exception;
+
+public class C01Exception {
+	public static void main(String[] args) {
+		// 일반예외 (Exception, checked exception)
+		// Exception 클래스와 그 하위 클래스의 객체
+		
+		// 실행예외 (RuntimeException, unchecked exception)
+		// RuntimeException 클래스와 그 하위 클래스의 객체
+		
+		
+		
+		
+	}
+}
