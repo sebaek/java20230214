82b1024c913266cc89444c68057ae0cb52975374
SeungEun Baek <sebaek@gmail.com>
2023-03-21 10:51:35 +0900

    catch 상위 타입 exception

---
+++ src/ch11/lecture/p02try_catch/C05MutipleException.java
@@ -0,0 +1,27 @@
+package ch11.lecture.p02try_catch;
+
+public class C05MutipleException {
+	public static void main(String[] args) {
+		try {
+			int[] a = {0, 1, 2};
+			
+			// ArithmeticException
+			// ArrayIndexOutOfBoundsException
+			int c = 3 / a[3];
+			
+			// 여러 exception 발생할 수 있는 경우
+			// 여러 catch block 작성 가능
+		} catch (ArithmeticException e) {
+			e.printStackTrace();
+		} catch (ArrayIndexOutOfBoundsException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("continue.....");
+	}
+}
+
+
+
+
+
---
+++ src/ch11/lecture/p02try_catch/C06MutipleException.java
@@ -0,0 +1,18 @@
+package ch11.lecture.p02try_catch;
+
+public class C06MutipleException {
+	public static void main(String[] args) {
+		try {
+			int[] a = {0, 1};
+			
+			// ArithmeticException
+			// ArrayIndexOutOfBoundsException
+			int c = 3 / a[2];
+			
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("continue....");
+	}
+}
