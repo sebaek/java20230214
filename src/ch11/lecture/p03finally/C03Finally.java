2e189654aca32cbc626d14ec75c20baf744610d7
SeungEun Baek <sebaek@gmail.com>
2023-03-21 11:53:43 +0900

    finally

---
+++ src/ch11/lecture/p03finally/C03Finally.java
@@ -0,0 +1,15 @@
+package ch11.lecture.p03finally;
+
+public class C03Finally {
+	public static void main(String[] args) {
+		try {
+			boolean a = true;
+			if (a) {
+				return;
+			}
+			
+		} finally {
+			System.out.println("finally block");
+		}
+	}
+}
