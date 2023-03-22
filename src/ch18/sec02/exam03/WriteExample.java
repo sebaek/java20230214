f97bb40abb7daa0de3af19e299c94fed4d4dcf6e
SeungEun Baek <sebaek@gmail.com>
2023-03-22 11:26:47 +0900

    write

---
+++ src/ch18/sec02/exam03/WriteExample.java
@@ -0,0 +1,18 @@
+package ch18.sec02.exam03;
+
+import java.io.*;
+
+public class WriteExample {
+	public static void main(String[] args) {
+		try (OutputStream os = new FileOutputStream("output/test3.db");) {
+
+			byte[] array = { 10, 20, 30, 40, 50 };
+
+			os.write(array, 1, 3);
+
+			os.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
