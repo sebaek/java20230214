c694bee0553f6310d764a118461050612cb64a65
SeungEun Baek <sebaek@gmail.com>
2023-03-22 16:31:48 +0900

    buffered outputstream

---
+++ src/ch18/sec04/exam02/ReadExample.java
@@ -0,0 +1,37 @@
+package ch18.sec04.exam02;
+
+import java.io.*;
+
+public class ReadExample {
+	public static void main(String[] args) {
+		try {
+			Reader reader = null;
+
+			reader = new FileReader("output/writer2.txt");
+			while (true) {
+				int data = reader.read();
+				if (data == -1) {
+					break;
+				}
+				System.out.println((char) data);
+			}
+
+			reader.close();
+			System.out.println();
+
+			reader = new FileReader("output/writer2.txt");
+			char[] data = new char[100];
+			while (true) {
+				int num = reader.read(data);
+				if (num == -1)
+					break;
+				for (int i = 0; i < num; i++) {
+					System.out.print(data[i]);
+				}
+			}
+			reader.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
