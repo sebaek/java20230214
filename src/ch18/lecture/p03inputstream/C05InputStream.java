97c7bb6cf213f5967bc85f3a92be369a07910909
SeungEun Baek <sebaek@gmail.com>
2023-03-22 12:40:21 +0900

    파일 복사

---
+++ src/ch18/lecture/p03inputstream/C05InputStream.java
@@ -0,0 +1,25 @@
+package ch18.lecture.p03inputstream;
+
+import java.io.*;
+
+public class C05InputStream {
+	public static void main(String[] args) {
+		String src = "output/slamdunk.jpg";
+		String des = "output/slamdunk_copy.jpg";
+
+		try (
+				var is = new FileInputStream(src);
+				var os = new FileOutputStream(des);) {
+
+			int data = 0;
+			
+			while ((data = is.read()) != -1) {
+				os.write(data);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("복사완료");
+	}
+}
