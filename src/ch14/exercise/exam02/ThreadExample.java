d85957ce3a7f5b0906bdea220d689fd293abf8ec
SeungEun Baek <sebaek@gmail.com>
2023-03-23 16:38:16 +0900

    확인문제2

---
+++ src/ch14/exercise/exam02/ThreadExample.java
@@ -0,0 +1,27 @@
+package ch14.exercise.exam02;
+
+public class ThreadExample {
+	public static void main(String[] args) {
+		Thread thread1 = new MovieThread();
+		
+		thread1.start();
+		
+		Thread thread2 = new Thread(new MusicRunnable());
+		
+		thread2.start();
+		
+		for (int i = 0; i < 3; i++) {
+			System.out.println("메인 쓰레드!!");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+
+
+
