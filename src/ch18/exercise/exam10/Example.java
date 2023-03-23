86793bfc6ed1be0a56b838ff70e23a6681faa0fa
SeungEun Baek <sebaek@gmail.com>
2023-03-23 12:14:08 +0900

    확인문제10

---
+++ src/ch18/exercise/exam10/Example.java
@@ -0,0 +1,12 @@
+package ch18.exercise.exam10;
+
+public class Example {
+	public static void main(String[] args) {
+		
+		// 파일명 입력 받는 코드(option)
+		String origin = "output/slamdunk.jpg";
+		String copy = "output/slamdunk_copy10.jpg";
+		
+		// 복사하는 코드 작성
+	}
+}
