2b0c97ac876ba76e8683f550baa02f672778e0d9
SeungEun Baek <sebaek@gmail.com>
2023-03-23 11:20:06 +0900

    byte to char

---
+++ src/cha18/sec06/CharacterConvertStreamExample.java
@@ -0,0 +1,35 @@
+package cha18.sec06;
+
+import java.io.*;
+
+public class CharacterConvertStreamExample {
+	public static void main(String[] args) throws Exception {
+		write("문자 변환 스트림을 사용합니다.");
+		String data = read();
+		System.out.println(data);
+	}
+
+	private static String read() throws Exception {
+		InputStream is = new FileInputStream("output/test.txt");
+		Reader reader = new InputStreamReader(is, "utf-8");
+		
+		char[] data = new char[100];
+		int num = reader.read(data);
+		
+		reader.close();
+		
+		return new String(data, 0, num);
+	}
+
+	private static void write(String str) throws Exception {
+		OutputStream os = new FileOutputStream("output/test.txt");
+		Writer writer = new OutputStreamWriter(os, "utf-8");
+		writer.write(str);
+		
+		writer.flush();
+		writer.close();
+	}
+}
+
+
+
