8f6335b6eece9367a24ce0592f753dbe8419c30b
SeungEun Baek <sebaek@gmail.com>
2023-03-23 10:28:20 +0900

    print

---
+++ src/ch18/lecture/p07print/C02PrintStream.java
@@ -0,0 +1,25 @@
+package ch18.lecture.p07print;
+
+import java.io.*;
+
+public class C02PrintStream {
+	public static void main(String[] args) {
+		String name = "output/print2.txt";
+
+		try (
+				PrintStream pw = new PrintStream(name);) {
+			
+			pw.println(99999);
+			pw.println(9.9999999);
+			pw.println(false);
+			pw.println("hello world");
+			pw.println("더 글로리");
+			
+			System.out.println();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
