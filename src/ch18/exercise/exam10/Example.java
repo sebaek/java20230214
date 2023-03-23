fa584b6ea6c18a48a6103c0a226b279da01e84c6
SeungEun Baek <sebaek@gmail.com>
2023-03-23 14:15:47 +0900

    copy

--- src/ch18/exercise/exam10/Example.java
+++ src/ch18/exercise/exam10/Example.java
@@ -1,12 +1,33 @@
 package ch18.exercise.exam10;
 
+import java.io.*;
+
 public class Example {
 	public static void main(String[] args) {
-		
+
 		// 파일명 입력 받는 코드(option)
 		String origin = "output/slamdunk.jpg";
 		String copy = "output/slamdunk_copy10.jpg";
-		
+
 		// 복사하는 코드 작성
+		try (
+				InputStream is = new FileInputStream(origin);
+				OutputStream os = new FileOutputStream(copy);
+
+				BufferedInputStream bis = new BufferedInputStream(is);
+				BufferedOutputStream bos = new BufferedOutputStream(os);) {
+
+			byte[] data = new byte[1024];
+			int len = 0;
+			while ((len = bis.read(data)) != -1) {
+				bos.write(data, 0, len);
+			}
+
+			bos.flush();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 }
