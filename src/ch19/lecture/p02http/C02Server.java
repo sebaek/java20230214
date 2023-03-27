a1f80d4b57da51468fe01f413c76f440866eb741
SeungEun Baek <sebaek@gmail.com>
2023-03-27 09:52:30 +0900

    http server

--- src/ch19/lecture/p02http/C02Server.java
+++ src/ch19/lecture/p02http/C02Server.java
@@ -17,12 +17,33 @@ public class C02Server {
 
 					OutputStream os = socket.getOutputStream();
 					PrintStream ps = new PrintStream(os);) {
+				
+				String body = """
+						<h1>This is NAVER!!</h1>
+						<h3>다른 사이트로 이동 </h3>
+						<ul>
+							<li><a href="https://www.daum.net">daum</a></li>
+							<li><a href="https://www.google.com">google</a></li>
+						</ul>
+						""";
+				int length = body.getBytes().length;
 
+				// 첫 줄
+				ps.println("HTTP/1.1 200 OK");
+				
+				// 두번째 줄부터 헤더들
+				ps.println("Content-Type: text/html; charset=utf-8");
+				ps.println("Content-Length: " + length);
+				
+				// 빈 줄
+				ps.println();
+				
+				// 응답 본문
+				ps.println(body);
+				
+
+				ps.flush();
 			}
-
-			ps.println("HTTP/1.1 200 OK");
-
-			ps.flush();
 
 		} catch (Exception e) {
 			e.printStackTrace();
